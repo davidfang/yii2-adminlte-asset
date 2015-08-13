4d573fe2de1f3a1e85500f56210553fdb4e02a83
davidfang <mw_fang@163.com>
2015-08-13 14:31:54 +0800

    修改用户名显示

--- example-views/yiisoft/yii2-app/layouts/left.php
+++ example-views/yiisoft/yii2-app/layouts/left.php
@@ -12,7 +12,7 @@ use yii\widgets\Menu;
                 <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p><?=Yii::$app->user->username?></p>
+                <p><?=Yii::$app->user->identity->username?></p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
